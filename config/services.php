0aa57015777862edd582fc6db51bb6c3e344bebb
mdwiastika <marceldwias@gmail.com>
2023-02-23 23:36:06 +0700

    repair github login

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     ],
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
-        'client-secret' => env('GITHUB_CLIENT_SECRET'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
         'redirect' => 'http://127.0.0.1:8000/auth/github/call-back',
     ],
 
